0dbcbb6f9d911665b2931410943dcc0caa7cc344
Reza1500 <rmohammadnia1378@gmail.com>
2017-06-08 07:35:22 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {184905857,152513900},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
